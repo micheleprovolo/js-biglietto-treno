d39f25cb9ee014fbefe30c1dd3c07f236895356e
micheleprovolo <michele.provolo@gmail.com>
2019-10-17 16:02:22 +0200

    impostazione esercizio

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// EX: calcolo prezzo biglietto del treno
+
+// dichiarazione variabili
+var nKm,nAnni,prezzoBigl,costoAlKm;
+
+//chiedo utente quanti km deve fare
+var nKm = prompt("Ciao! Quanti km devi fare?");
+
+//chiedo utente quanti anni ha
+var nAnni = prompt("Quanti anni hai?");
+
+//costo al km
+var costoAlKm = 0.21 * 1;
+
+//calcolo prezzo biglietto in base ai km (0,21 euro/km)
+var prezzoBigl = nkm * costoAlKm;
+
+console.log(prezzoBigl);
+
+//output costo biglietto
