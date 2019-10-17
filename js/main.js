cb7bfeb3568e0d886b4d7d26bab5b1a36c1a8e74
micheleprovolo <michele.provolo@gmail.com>
2019-10-17 18:12:55 +0200

    sembrerebbe funzionare

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 //Il prezzo del biglietto è definito in base ai km (0.21 €cent al km) ma c’è uno sconto del 20% per i minorenni e del 40% per gli over 65
 
 // dichiarazione variabili
-var nKm,nAnni,prezzoBigl,costoAlKm;
+var nKm,nAnni,prezzoBigl,costoAlKm,sconto,costoDef;
 
 //chiedo utente quanti km deve fare
 nKm = parseInt (prompt("Ciao! Quanti km devi fare?"));
@@ -15,18 +15,28 @@ nAnni = prompt("Quanti anni hai?");
 //costo al km
 costoAlKm = 0.21 * 1;
 
-//verifico se utente ha meno di 18 anni
+//calcolo prezzo biglietto in base ai km (0,21 euro/km)
+prezzoBigl = nKm * costoAlKm;
+
+
+//verifico se utente ha meno di 18 anni o più-uguale a di 65 anni per attribuire eventuale sconto sul totale
 if (nAnni < 18) {
-    prezzoBigl - 20%;
+   sconto = prezzoBigl / 100 * 20;
   }
 
 else if (nAnni >= 65) {
-    prezzoBigl - 40%;
+    sconto = prezzoBigl / 100 * 40;
 }
 
+else {
+    sconto = 0;
+}
 
-//calcolo prezzo biglietto in base ai km (0,21 euro/km)
-prezzoBigl = nKm * costoAlKm;
+costoDef = prezzoBigl - sconto;
+
+console.log(costoDef);
+
+
 
 //console.log(prezzoBigl);
 
